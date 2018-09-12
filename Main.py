16562b1fe9fac716d7d93aa7bec8334a3d54cc1f
Ivo Pontes <ivopontes@outlook.com>
2018-09-11 23:02:47 -0300

    Histograma funcional

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ from Histograma import Histograma
 if __name__ == '__main__':
 	print("Processamento de Imagens.")
 
-	histograma = Histograma("img/lena.png")
+	histograma = Histograma("img/4x4.png")
 	histograma.carregarImagem()
 
 	histograma.executar()
